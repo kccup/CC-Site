1adde5d1a390e8d95222a9ef01df7c0e872fb6da
Symere <159564961+kccup@users.noreply.github.com>
2024-04-07 22:14:05 -0500

    kawaii

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-const changeBackgroundButton = document.getElementById('changeBackground');
-
-changeBackgroundButton.addEventListener('click', () => {
-    const body = document.body;
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    body.style.backgroundColor = randomColor;
-});
-
-let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-let counter = 0;
-
-document.getElementById('changeBackground').addEventListener('click', function() {
-    document.body.style.backgroundColor = colors[counter];
-    counter = (counter + 1) % colors.length;
-});
+window.onload = function() {
+    document.getElementById('changeBackground').addEventListener('click', function() {
+        var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = randomColor;
+    });
+};
